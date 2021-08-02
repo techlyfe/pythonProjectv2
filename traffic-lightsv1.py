e356839567421942acce88da8124af99f43160ba
Scott Barrett <scottbarrett64@gmail.com>
2021-08-02 14:49:50 +1000

    testing v1

--- traffic-lightsv1.py
+++ traffic-lightsv1.py
@@ -1,18 +1,23 @@
 from gpiozero import LED
+from gpiozero import Button
 from time import sleep
 
 red = LED(22)
 amber = LED(27)
 green = LED(17)
+on_switch = Button(13)
+
 
 while True:
-    red.on()
-    sleep(10)
-    red.off()
-    amber.on()
-    sleep(3)
-    amber.off()
-    green.on()
-    sleep(10)
-    green.off()
-
+    if button.is_pressed:
+        red.on()
+        sleep(1)
+        red.off()
+        amber.on()
+        sleep(2)
+        amber.off()
+        green.on()
+        sleep(1)
+        green.off()
+    else:
+        print("Waiting on Button to be pressed")
