cd6970097302ad2bd0cf962cf712ec77ddaab8e0
Scott Barrett <scottbarrett64@gmail.com>
2021-08-02 14:34:41 +1000

    testing v1

--- traffic-lightsv1.py
+++ traffic-lightsv1.py
@@ -1,11 +1,17 @@
 from gpiozero import LED
 from time import sleep
 
-red = LED(17)
-blue = LED(18)
+red = LED(22)
+amber = LED(27)
+green = LED(17)
 
 while True:
     red.on()
     sleep(1)
     red.off()
+    amber.on()
     sleep(1)
+    amber.off()
+    green.on()
+    sleep(1)
+    green.off()
